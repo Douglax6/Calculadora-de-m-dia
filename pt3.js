baf33848b399ac44cd141f7538b676a6d0a28ec9
Douglas Evaristo <100808078+Douglax6@users.noreply.github.com>
2022-03-13 18:13:48 -0300

    Add files via upload

---
+++ pt3.js
@@ -0,0 +1,16 @@
+var name = "Douglas!"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre =  4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre+
+    notaDoQuartoBimestre) /4
+
+var notaFixada = notaFinal.toFixed(1) 
+
+console.log("Bem Vindo " + name)
+console.log(notaFixada)
+
+// Revisão
+//Variáveis, strings, console.log, toFixed, operações matemáticas: + - * /, concatenação
